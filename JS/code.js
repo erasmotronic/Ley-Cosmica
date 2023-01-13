6cf3b78ccb7bee235412b0a805777fc59647fe43
erasmotronic <115958159+erasmotronic@users.noreply.github.com>
2023-01-13 10:30:51 -0600

    Add files via upload

---
+++ JS/code.js
@@ -0,0 +1,43 @@
+$(function() {
+
+    $("#blackSunDawn").click(function() {
+        $("#blackSunContent").slideToggle();
+        $("#blackSunDawn").hide();
+    });
+
+    $("#strikeBlaze").click(function() {
+        $("#strikeBlazeContent").slideToggle();
+        $("#strikeBlaze").hide();
+    });
+
+    $("#whyCosmicLawEin").click(function() {
+        $("#whyCosmicLawEinContent").slideToggle();
+        $("#whyCosmicLawEin").hide();
+    });
+
+    $("#aviesoPoder").click(function() {
+        $("#aviesoPoderContent").slideToggle();
+        $("#aviesoPoder").hide();
+    });
+
+    $("#goblinoUgly").click(function() {
+        $("#goblinoUglyContent").slideToggle();
+        $("#goblinoUgly").hide();
+    });
+
+    $("#laTimidez").click(function() {
+        $("#laTimidezContent").slideToggle();
+        $("#laTimidez").hide();
+    });
+
+    $("#CBDCGridlock").click(function() {
+        $("#CBDCGridlockContent").slideToggle();
+        $("#CBDCGridlock").hide();
+    });
+
+    $("#sexClowns").click(function() {
+        $("#sexClownsContent").slideToggle();
+        $("#sexClowns").hide();
+    });
+
+});
